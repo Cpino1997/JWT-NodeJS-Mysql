192d239e6bdc5d01928fb913421a2107b271c41c
PinoLabs <pino440@gmail.com>
2022-10-04 02:03:29 -0300

    Update Final

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE,
+})
+
+conn.connect( (error)=>{
+    if(error){
+        console.log('El error es :'+error)
+        return
+    }
+    console.log('Contectado!')
+})
+
+module.exports = conn
